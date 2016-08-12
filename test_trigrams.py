5c30f1a7493de02006dd4c6d8a1f0738a965f489
David Smith <dbsmith.dbs83@gmail.com>
2016-08-11 22:19:22 -0700

    improved test func indenting commenting

--- test_trigrams.py
+++ test_trigrams.py
@@ -1,13 +1,15 @@
+"""Test module for trigrams."""
 import pytest
 
 WORD_BANK_TABLE = [
     (['word1', 'word2', 'word3', 'word4', 'word5', 'word6'],
         {'word1 word2': ['word3'], 'word2 word3': ['word4'],
-            'word3 word4': ['word5'], 'word4 word5': ['word6']}),
+         'word3 word4': ['word5'], 'word4 word5': ['word6']}),
 ]
 
 
 @pytest.mark.parametrize('word_list, result', WORD_BANK_TABLE)
 def test_create_word_bank(word_list, result):
+    """Test the word bank builder function."""
     from trigrams import create_word_bank
     assert create_word_bank(word_list) == result
